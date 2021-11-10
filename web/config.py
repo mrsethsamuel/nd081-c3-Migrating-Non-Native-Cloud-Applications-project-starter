59088a0c21b3692d8ee3dff767e0591e20d32693
Seth Samuel <Samset2305@gmail.com>
2021-11-10 21:46:47 +0100

    Updated Config

--- web/config.py
+++ web/config.py
@@ -11,11 +11,11 @@ class BaseConfig:
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
-    SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
-    SERVICE_BUS_CONNECTION_STRING ='' #TODO: Update value
+    SECRET_KEY = 'mn3n+8ykeL9KPMXCq8EKQVRyj9pilBVG7YwoJAlKd3c='
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://notificationqueue.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=mn3n+8ykeL9KPMXCq8EKQVRyj9pilBVG7YwoJAlKd3c=' #TODO: Update value
     SERVICE_BUS_QUEUE_NAME ='notificationqueue'
     ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
-    SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
+    SENDGRID_API_KEY = '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' #Configuration not required, required SendGrid Account
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
